52cf3f97bc6fe6bd179225b3176eb0ccf32d9ce8
Jacob Cargen <jacobcargen@gmail.com>
2022-04-13 21:29:29 -0400

    Add project files.

---
+++ KSPController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		KSPController.ino
+ Created:	04/13/2022 21:27:40
+ Author:	jacob
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
